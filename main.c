9da43430458d7d9a9fee7c6f92bdc981449bf8c2
Nelg <472713985@qq.com>
2017-11-30 19:52:52 +0800

    update

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+
+	//连接套接字编号
+	int socketfd;
+	
+	//获取服务器地址和端口
+	char *serverIp = "127.0.0.1";
+	char *serverPort = "9908";
+	
+
+	return 0;
+}
