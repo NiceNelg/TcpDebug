49991f8699d17a695870333849cd96a0983e6c4a
Nelg <472713985@qq.com>
2017-12-16 16:50:15 +0800

    upate

--- display.c
+++ display.c
@@ -3,10 +3,16 @@
 //初始化屏幕
 void display_init(display_win **win) {
 	//初始化窗口结构体
-	*win = (display_win *)malloc( sizeof(WINDOW) * 3 );
+	*win = (display_win *)malloc( sizeof(display_win *) );
+	(*win)->win_send = (WINDOW *)malloc(sizeof(WINDOW));
+	(*win)->win_revice = (WINDOW *)malloc(sizeof(WINDOW));
+	(*win)->win_command = (WINDOW *)malloc(sizeof(WINDOW));
+
 	//开启curses库
 	initscr();
 	//开启键盘特殊字元输入
 	keypad(stdscr, true);
+
+
 
 }
