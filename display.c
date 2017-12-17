e09839288d57c823ef4937ce799c646db36fc986
Nelg <472713985@qq.com>
2017-12-17 23:50:07 +0800

    update

--- display.c
+++ display.c
@@ -2,17 +2,17 @@
 
 //获取屏幕信息
 void static display_getwindosinfo( display_win *win ) {
-	ioctl(STDIN_FILENO, TIOCGWINSZ, &(*win).win_info);
+	ioctl(STDIN_FILENO, TIOCGWINSZ, &(*win).terminal_info);
 }
 
 //初始化屏幕
 void display_init(display_win **win) {
 	//初始化窗口结构体
-	*win = (display_win *)malloc( sizeof(display_win *) * 3 + sizeof(struct winsize) );
-	(*win)->win_send = (WINDOW *)malloc(sizeof(WINDOW));
-	(*win)->win_revice = (WINDOW *)malloc(sizeof(WINDOW));
-	(*win)->win_command = (WINDOW *)malloc(sizeof(WINDOW));
-
+	*win = (display_win *)malloc( (sizeof(display_win_info) + sizeof(WINDOW)) * 3 + sizeof(struct winsize) );
+	(*win)->win_send.index = (WINDOW *)malloc(sizeof(WINDOW));
+	(*win)->win_revice.index = (WINDOW *)malloc(sizeof(WINDOW));
+	(*win)->win_command.index = (WINDOW *)malloc(sizeof(WINDOW));
+	
 	//获取当前终端信息
 	display_getwindosinfo(*win);
 	
@@ -23,23 +23,28 @@ void display_init(display_win **win) {
 
 	//新窗口(行，列,begin_y,begin_x) 
 	//建立子窗口
-	(*win)->win_send = derwin(
+	(*win)->win_send.index = derwin(
 			stdscr,
-			(int)( (*win)->win_info.ws_row * 0.7 ), 
-			(int)( (*win)->win_info.ws_col / 2 ), 
+			(int)( (*win)->terminal_info.ws_row * 0.8 ), 
+			(int)( (*win)->terminal_info.ws_col / 2 ), 
 			0,
 			0
 	);
 	
-	box( (*win)->win_send,ACS_VLINE,ACS_HLINE); 
+	box( (*win)->win_send.index,ACS_VLINE,ACS_HLINE); 
 
-  mvwprintw( (*win)->win_send, 1, 1, "send windows"); 
+  mvwprintw( (*win)->win_send.index, 1, 1, "send windows"); 
 
-  mvwprintw( (*win)->win_send,2,1,"www.deepfuturesoft.info");
+  mvwprintw( (*win)->win_send.index,2,1,"www.deepfuturesoft.info");
 
 	refresh();
-	wrefresh((*win)->win_send);
+	wrefresh((*win)->win_send.index);
 	//touchwin((*win)->win_send);
+
+}
+
+//建立发送窗口
+void static display_init_send(display_win *win) {
 
 }
 
@@ -47,20 +52,20 @@ void display_init(display_win **win) {
 void display_del(display_win *win, const char *member) {
 	//筛选成员
 	if( strcmp(member,"win_send") == 0 ) {
-		delwin(win->win_send);
+		delwin(win->win_send.index);
 	}else if( strcmp(member, "win_revice") == 0 ) {
-		delwin(win->win_revice);
+		delwin(win->win_revice.index);
 	}else if( strcmp(member, "win_command") == 0 ) {
-		delwin(win->win_command);
+		delwin(win->win_command.index);
 	}
 }
 
 //结束屏幕显示
 void display_end(display_win **win) {
 	//删除所有屏幕
-	delwin( (*win)->win_send );
-	delwin( (*win)->win_revice );
-	delwin( (*win)->win_command );
+	delwin((*win)->win_send.index);
+	delwin((*win)->win_revice.index);
+	delwin((*win)->win_command.index);
 
 	//结束屏幕
 	endwin();
--- display.h
+++ display.h
@@ -10,16 +10,32 @@
 #include <unistd.h>
 #include <stdio.h>
 
+//定义窗口信息结构体
+typedef struct display_win_info {
+	//curses库WINDOW序号
+	WINDOW *index;
+	//窗口所占行数
+	unsigned short int row;
+	//窗口所占列数
+	unsigned int col;
+}display_win_info;
+
 //定义窗口结构体
 typedef struct win {
+	//发送提示窗口
+	display_win_info win_tip_send;
 	//发送窗口
-	WINDOW *win_send;
+	display_win_info win_send;
+	//接收提示窗口
+	display_win_info win_tip_revice;
 	//接收窗口
-	WINDOW *win_revice;
+	display_win_info win_revice;
+	//命令提示窗口
+	display_win_info win_tip_command;
 	//命令窗口
-	WINDOW *win_command;
+	display_win_info win_command;
 	//终端窗口信息
-	struct winsize win_info;
+	struct winsize terminal_info;
 }display_win;
 
 //获取屏幕信息
