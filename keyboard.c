c9948883b9e80b4c3c549c12624d461ac766260c
NiceNelg <472713985@qq.com>
2018-03-18 18:19:28 +0800

    update

--- keyboard.c
+++ keyboard.c
@@ -1,5 +1,8 @@
 #include "keyboard.h"
 
 void keyboard_init() {
+	//获取输入串口（键盘的串口文件符为0）的配置
+	tcgetattr(0, &keyboard_setting);
+	//创建新的串口配置
 	
 }
--- keyboard.h
+++ keyboard.h
@@ -4,7 +4,13 @@
 #include <stdio.h>
 #include <termios.h>
 
+static struct termios keyboard_setting, keyboard_new_setting;
+static int keyboard_character = -1;
+
 void keyboard_init();
 void keyboard_close();
+
+int keyboard_hit();
+int keyboard_readch();
 
 #endif
